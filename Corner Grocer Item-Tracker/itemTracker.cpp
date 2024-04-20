b604cfe97c09f218995872ed376b3f14fad3636f
Lnginyard <lnginyard@gmail.com>
2024-04-19 23:06:12 -0400

    did some modifying in itemTracker class

--- Corner Grocer Item-Tracker/itemTracker.cpp
+++ Corner Grocer Item-Tracker/itemTracker.cpp
@@ -3,35 +3,37 @@
 #include <map>
 #include <limits>
 #include <string>
+//Include necessary libraries
+
 
 using namespace std;
-
+//Item tracker class
 class ItemTracker {
 private:
     map<string, int> itemFrequency;
-
+    //Read input file
 public:
     void readInputFile() {
         ifstream inputFile("CS210_Project_Three_Input_File.txt");
         string item;
-
+        
         while (inputFile >> item) {
             itemFrequency[item]++;
         }
 
         inputFile.close();
     }
-
+    //Get item frequency
     int getItemFrequency(const string& item) {
         return itemFrequency[item];
     }
-
+    //Print item frequencies
     void printItemFrequencies() {
         for (const auto& pair : itemFrequency) {
             cout << pair.first << " " << pair.second << endl;
         }
     }
-
+    //Print item histogram
     void printItemHistogram() {
         for (const auto& pair : itemFrequency) {
             cout << pair.first << " ";
@@ -41,7 +43,7 @@ public:
             cout << endl;
         }
     }
-
+    //Create backup file
     void createBackupFile() {
         ofstream backupFile("frequency.dat");
 
@@ -52,7 +54,7 @@ public:
         backupFile.close();
     }
 };
-
+//Get menu choice from user
 int getMenuChoice() {
     int choice;
     cout << "Menu Options:" << endl;
@@ -62,30 +64,30 @@ int getMenuChoice() {
     cout << "4. Exit" << endl;
     cout << "Enter your choice: ";
     cin >> choice;
-
+    //Display error msg if user selects invalid option
     while (cin.fail() || choice < 1 || choice > 4) {
         cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid choice. Please enter a valid menu option: ";
         cin >> choice;
     }
 
     return choice;
 }
-
+//Main function
 int main() {
     ItemTracker tracker;
     tracker.readInputFile();
-
+    
     int choice;
     do {
         choice = getMenuChoice();
-
+       
         switch (choice) {
         case 1: {
             string item;
             cout << "Enter the item to look up: ";
-            std::getline(std::cin, item); //use this instead of cin >> item
+            cin, item;
             int frequency = tracker.getItemFrequency(item);
             cout << "Frequency of " << item << ": " << frequency << endl;
             break;
